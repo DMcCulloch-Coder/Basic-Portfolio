8727bf22ef07f8a8becb3e6c6850a1df812cc10b
David <DMcCulloch86@gmail.com>
2019-09-12 21:31:22 -0400

    added form info to contact page and added js button functionality added social media icons to index.html

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+document.getElementById('submit').onclick = function (){
+   
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    alert('ERROR! Please use e-mail link to contact user.')
+
+    //console.log(name + email + message)
+
+    //find a way to export this data
+
+
+}
+
+
